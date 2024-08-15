f6729130c09e6f5ec53a5a81366289d6453b3152
IbnuJabir <kedirjabir12@gmail.com>
2024-08-15 21:22:29 +0300

    add documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+"""
+Solution to the lockboxes problem.
+"""
+
 def canUnlockAll(boxes):
-    keys = [0]
-    for key in keys:
-        for new_key in boxes[key]:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    """
+    Determines if all boxes can be opened with the keys available.
+    
+    Args:
+        boxes (list of lists): Each sublist contains the keys available in a box.
+    
+    Returns:
+        bool: True if all boxes can be opened, otherwise False.
+    """
+    keys = set([0])  # Set of keys to open boxes, starting with box 0 opened.
+    queue = [0]      # Queue for managing the order of boxes to check.
+    
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in keys:
+                keys.add(key)
+                queue.append(key)
+    
+    return len(keys) == len(boxes)
