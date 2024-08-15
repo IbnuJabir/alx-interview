2b00ec7bb15058d17519a7da524a1fe3eba48553
IbnuJabir <kedirjabir12@gmail.com>
2024-08-15 21:39:28 +0300

    update the function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Solution to the lockboxes problem.
+Solution to lockboxes problem
 """
+
 
 def canUnlockAll(boxes):
     """
@@ -9,14 +10,17 @@ def canUnlockAll(boxes):
     based on keys that can be attained.
     Solution to the lockboxes problem
     """
-    keys = set([0])
-    queue = [0]
-    
-    while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if key < len(boxes) and key not in keys:
-                keys.add(key)
-                queue.append(key)
-    
-    return len(keys) == len(boxes)
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
