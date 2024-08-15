b7feeecd3b2ce2728d1a013e8c2882536c940579
IbnuJabir <kedirjabir12@gmail.com>
2024-08-15 21:26:09 +0300

    document the function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,11 +7,12 @@ def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened with the keys available.
     
-    Args:
-        boxes (list of lists): Each sublist contains the keys available in a box.
+    Parameters:
+    boxes (list of list of int): A list where each sublist contains the keys
+                                 available in that particular box.
     
     Returns:
-        bool: True if all boxes can be opened, otherwise False.
+    bool: True if all boxes can be opened, False otherwise.
     """
     keys = set([0])  # Set of keys to open boxes, starting with box 0 opened.
     queue = [0]      # Queue for managing the order of boxes to check.
