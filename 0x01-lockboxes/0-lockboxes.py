bfa07ded352ca0ee0bcdb212e047f4b81bfda47d
IbnuJabir <kedirjabir12@gmail.com>
2024-08-15 21:29:04 +0300

    remove unneccesary functions

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,12 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened with the keys available.
     
     Parameters:
-    boxes (list of list of int): A list where each sublist contains the keys
-                                 available in that particular box.
+    boxes (list of list of int): A list where each sublist contains the keys available in that particular box.
     
-    Returns:
-    bool: True if all boxes can be opened, False otherwise.
+    Returns: bool: True if all boxes can be opened, False otherwise.
     """
-    keys = set([0])  # Set of keys to open boxes, starting with box 0 opened.
-    queue = [0]      # Queue for managing the order of boxes to check.
+    keys = set([0])
+    queue = [0]
     
     while queue:
         current_box = queue.pop(0)
